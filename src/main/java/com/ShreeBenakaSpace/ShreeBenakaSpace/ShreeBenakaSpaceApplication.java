c28ab8458db6062a7f2c5123cd4696f703afc23d
sample <amdeepasanathshashwath@gmail.com>
2026-01-28 15:05:19 +0530

    ShreeBenakaSpaceWelcomePage

---
+++ src/main/java/com/ShreeBenakaSpace/ShreeBenakaSpace/ShreeBenakaSpaceApplication.java
@@ -0,0 +1,13 @@
+package com.ShreeBenakaSpace.ShreeBenakaSpace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShreeBenakaSpaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShreeBenakaSpaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ShreeBenakaSpace/ShreeBenakaSpace/ShreeBenakaSpaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ShreeBenakaSpace.ShreeBenakaSpace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShreeBenakaSpaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
